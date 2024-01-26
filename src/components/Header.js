d37af1d8d43237985cfc7dc6432c302f88c4df7f
Eric Eriksson <ericeriksson@gmail.com>
2024-01-26 14:36:56 -0500

    refactored mana symbols

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,11 +1,21 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import logo from "../assets/mana.png";
+import W from "../assets/W.svg";
+import U from "../assets/U.svg";
+import B from "../assets/B.svg";
+import R from "../assets/R.svg";
+import G from "../assets/G.svg";
 
 const Header = ({ title, subTitle }) => {
   return (
     <header className="App-header">
-      <img src={logo} className="logo" alt="mana symbols" />
+      <div className="mana-cost">
+        <img src={W} className="mana" alt="white mana symbol" />
+        <img src={U} className="mana" alt="blue mana symbol" />
+        <img src={B} className="mana" alt="black mana symbol" />
+        <img src={R} className="mana" alt="red mana symbol" />
+        <img src={G} className="mana" alt="green mana symbol" />
+      </div>
       <h1>
         <Link to="/" className="aethermind">
           {title}
